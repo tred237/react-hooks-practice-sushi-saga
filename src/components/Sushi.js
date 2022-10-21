760ec25bc3826bca0d772ceca62869a926c2ca6d
trevor dewindt <trevor.dewindt@gmail.com>
2022-10-21 14:33:22 -0600

    add finished code

--- src/components/Sushi.js
+++ src/components/Sushi.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Sushi(props) {
+function Sushi({ sushi, onPlatesClick, platesEaten, totalBudget }) {
+  const [eaten, setEaten] = useState(false)
+
+  function handleClick(){
+    const spend = platesEaten.reduce((pv,cv) => pv + cv.price, sushi.price)
+    if(!eaten && spend <= totalBudget){
+      setEaten(!eaten)
+      onPlatesClick(!eaten, sushi)
+    }
+    if(eaten){
+      setEaten(!eaten)
+      onPlatesClick(!eaten, sushi)
+    }
+  }
+
   return (
     <div className="sushi">
-      <div className="plate" onClick={/* Give me a callback! */ null}>
-        {/* Tell me if this sushi has been eaten! */}
-        {false ? null : (
+      <div className="plate" onClick={handleClick}>
+        {eaten ? null : (
           <img
-            src={/* Give me an image source! */ null}
-            alt={/* Give me a name! */ "Sushi"}
+            src={sushi.img_url}
+            alt={sushi.name}
             width="100%"
           />
         )}
       </div>
       <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
+        {sushi.name} - ${sushi.price}
       </h4>
     </div>
   );
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,13 @@
 import React from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({ sushiList, sushiAmount, limitSushi, onPlatesClick, platesEaten, totalBudget }) {
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiList.map(sushi => <Sushi key={sushi.id} sushi={sushi} onPlatesClick={onPlatesClick} platesEaten={platesEaten} totalBudget={totalBudget}/>)}
+      <MoreButton sushiAmount={sushiAmount} limitSushi={limitSushi}/>
     </div>
   );
 }
